cc48236160f99dc49e448954be436d7e5b4f2fe8
Wangxy <xtwxy@hotmail.com>
2018-07-31 11:29:41 +0800

    initial import.

---
+++ src/main/java/com/github/apue/ws/eventsource/Application.java
@@ -0,0 +1,23 @@
+package com.github.apue.ws.eventsource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
+
+@Configuration
+@ComponentScan({"com.github.apuex.ws.*"})
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+  @Bean
+  ProtobufHttpMessageConverter protobufHttpMessageConverter() {
+    return new ProtobufHttpMessageConverter();
+  }
+}
